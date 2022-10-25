f8839a81e79647642cb5a34189d5ddf8882c0f80
JJK70 <jjk701@gmail.com>
2022-10-25 18:31:27 -0400

    fix form changes

--- src/pages/ReviewList/ReviewList.jsx
+++ src/pages/ReviewList/ReviewList.jsx
@@ -1,18 +1,18 @@
 import styles from './ReviewList.module.css'
-import ReviewForm from '../../components/ReviewForm/ReviewForm'
+// import ReviewForm from '../../components/ReviewForm/ReviewForm'
 
 const ReviewList = (props) => {
   return (
     <>
       <h1>Reviews</h1>
-      <div className={styles.container}>
+      {/* <div className={styles.container}>
         {props.reviews.map(review =>
-          <ReviewList 
+          <ReviewForm 
             review={review} 
             key={review._id} 
           />
         )}
-      </div>
+      </div> */}
     </>
   );
 }
